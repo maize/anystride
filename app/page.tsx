0eaff82335863432a18c823e7d73fc6b10747808
Matthias Link <matthias.e.link@gmail.com>
2024-01-20 18:25:12 -0500

    Use AnyStride in title on landing page

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="w-full min-h-screen flex items-center justify-center relative isolate overflow-hidden bg-gray-900">
       <div className="mx-auto max-w-xl lg:max-w-lg">
-        <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">NYRR volunteering notifier</h2>
+        <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">AnyStride</h2>
         <p className="mt-4 text-lg leading-8 text-gray-300">
           Get notified about volunteering opportunities with NYRR for the 9+1 program.
         </p>
